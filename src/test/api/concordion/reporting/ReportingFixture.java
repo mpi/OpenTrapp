f7c874b470e5f37d2868bd6753db16300ad2ce68
mpi <mpi.michal.piotrkowski+gh@gmail.com>
2014-03-16 21:08:44 +0100

    fix concordion

--- src/test/api/concordion/reporting/ReportingFixture.java
+++ src/test/api/concordion/reporting/ReportingFixture.java
@@ -1,8 +1,5 @@
 package concordion.reporting;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import support.ApiFixture;
@@ -11,9 +8,9 @@ import com.github.mpi.time_registration.domain.EmployeeID;
 import com.github.mpi.time_registration.domain.ProjectName;
 import com.github.mpi.time_registration.domain.WorkLogEntry;
 import com.github.mpi.time_registration.domain.WorkLogEntry.EntryID;
-import com.github.mpi.time_registration.domain.time.Day;
 import com.github.mpi.time_registration.domain.WorkLogEntryRepository;
 import com.github.mpi.time_registration.domain.Workload;
+import com.github.mpi.time_registration.domain.time.Day;
 
 public class ReportingFixture extends ApiFixture {
 
@@ -24,4 +21,8 @@ public class ReportingFixture extends ApiFixture {
         repository.store(new WorkLogEntry(new EntryID(id), Workload.of(workload), new ProjectName(projectName), new EmployeeID(employee), Day.of(day)));
     }
 
+    public void workLogEntry(String id, String workload, String projectName, String employee) {
+        repository.store(new WorkLogEntry(new EntryID(id), Workload.of(workload), new ProjectName(projectName), new EmployeeID(employee), Day.of("2014/01/01")));
+    }
+    
 }
