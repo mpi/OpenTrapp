074a23e3c26d36b7b3e0b897f430d15816a37cbc
Adam Pierzchała <adam.pierzchala@pragmatists.pl>
2014-04-08 17:51:15 +0200

    Security feature: check if editing worklog is owned by user

--- src/main/java/com/github/mpi/time_registration/application/RegistrationEndpoint.java
+++ src/main/java/com/github/mpi/time_registration/application/RegistrationEndpoint.java
@@ -5,15 +5,19 @@ import com.github.mpi.time_registration.domain.RegistrationService;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
 public class RegistrationEndpoint {
@@ -29,6 +33,7 @@ public class RegistrationEndpoint {
             consumes = "application/json",
                value = "/endpoints/v1/employee/{employeeID}/work-log/entries")
     @ResponseStatus(CREATED)
+    @PreAuthorize("@registrationEndpoint.isCurrentEmployee(#employeeID)")
     public void submitEntry(@PathVariable String employeeID, @RequestBody Form form) {
 
         try{
@@ -47,15 +52,20 @@ public class RegistrationEndpoint {
         }
     }
 
+    public boolean isCurrentEmployee(String employeeID) throws IOException {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        return auth.getName().equals(employeeID);
+    }
 
     @JsonAutoDetect(fieldVisibility=Visibility.ANY)
-    static class Form{
+    public static class Form{
 
         String projectName, workload, day;
     }
 
     @ResponseStatus(BAD_REQUEST)
-    class InvalidExpressionException extends IllegalArgumentException {
+    public class InvalidExpressionException extends IllegalArgumentException {
         
         private static final long serialVersionUID = -981128848209981239L;
         
---
+++ src/main/java/com/github/mpi/users_and_access/infrastructure/spring/CustomAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.github.mpi.users_and_access.infrastructure.spring;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("application/vnd.captech-v1.0+json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        PrintWriter out = response.getWriter();
+        out.print("{\"message\":\"You are not privileged to request this resource.\", \"access-denied\":true,\"cause\":\"AUTHORIZATION_FAILURE\"}");
+        out.flush();
+        out.close();
+    }
+}
