b6b1d20718173718ba966b9917ba9bccb0664bd3
mpi <mpi.michal.piotrkowski+gh@gmail.com>
2014-04-19 15:29:25 +0200

    mock security provider

--- src/main/java/com/github/mpi/infrastructure/DemoProfile.java
+++ src/main/java/com/github/mpi/infrastructure/DemoProfile.java
@@ -27,12 +27,12 @@ public class DemoProfile {
     @Autowired
     private WorkLogEntryRepository repository;
     
-    private Random random = new Random();;
+    private Random random = new Random();
 
     @PostConstruct
     void init(){
 
-        List<String> employees = Arrays.asList("homer.simpson", "bart.simpson", "marge.simpson", "principal skinner", "mr.burns");
+        List<String> employees = Arrays.asList("homer.simpson", "bart.simpson", "marge.simpson", "principal.skinner", "mr.burns");
         List<String> workloads = Arrays.asList("8h", "8h", "8h", "4h 15m", "2h 30m", "1d 2h", "45m", "1h", "6h 40m", "7h");
         List<String> projects = Arrays.asList("ProjectManhattan", "ApolloProgram", "Internal", "Other");
         String workloadPattern = "WL.%s";
